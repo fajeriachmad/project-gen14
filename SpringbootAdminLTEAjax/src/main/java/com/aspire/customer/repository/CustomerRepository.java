90222075f623afe0d45bae795991892eced23a8f
Fajri Rahman <fajri.achmad55@gmail.com>
2023-03-31 17:14:21 +0700

    Query update on repository.

--- SpringbootAdminLTEAjax/src/main/java/com/aspire/customer/repository/CustomerRepository.java
+++ SpringbootAdminLTEAjax/src/main/java/com/aspire/customer/repository/CustomerRepository.java
@@ -9,9 +9,9 @@ import org.springframework.data.repository.query.Param;
 import com.aspire.customer.model.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {	
-	@Query(value = "SELECT c FROM Customer c WHERE LOWER(c.name) LIKE LOWER('%' || :keyword || '%')"
-			+ " OR LOWER(c.email) LIKE LOWER('%' || :keyword || '%')"
-			+ " OR LOWER(c.city) LIKE LOWER('%' || :keyword || '%') ORDER BY c.id ASC")
+	@Query(value = "SELECT c FROM Customer c WHERE c.name ILIKE '%' || :keyword || '%'"
+			+ " OR c.email ILIKE '%' || :keyword || '%'"
+			+ " OR c.city ILIKE '%' || :keyword || '%' ORDER BY c.id ASC")
 	public List<Customer> search(@Param("keyword") String keyword);
 	
 	public List<Customer> findAllByOrderByIdAsc();
